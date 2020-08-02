abd2e3b9cf36f50fc5c0e1169c422c41c35caf96
Karter13 <KarMiH1975@mail.ru>
2020-08-02 21:55:24 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+// import './lessons/lesson2/lesson2';
+import './lessons/lesson6/lesson6';
+
+function App() {
+  return (
+    <h1>To connect your lesson, change the import path in the App.tsx!</h1>
+  );
+}
+
+export default App;
